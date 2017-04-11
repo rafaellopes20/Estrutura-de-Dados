8e9fb091c7d3b649d5afd68a4d3a1fc9a804017a
rafaellopes21 <rafaelsilva.1@univille.com>
2017-04-10 21:52:42 -0300

    pow2

--- Listas/Test/estd/SLinkedListTes.java
+++ Listas/Test/estd/SLinkedListTes.java
@@ -22,5 +22,22 @@ public class SLinkedListTes {
 		Assert.assertTrue(linkedList.getSize() == 3);
 		Assert.assertFalse(linkedList.isEmpty());
 	}
+	@Test
+	public void removeFirstTest(){
+		SLinkedList<String>linkedList = new SLinkedList<>();
+		Assert.assertTrue(linkedList.getSize() == 0);
+		Assert.assertTrue(linkedList.isEmpty()); // testando a classe booleana
+		
+		Node<String> nodeA = new Node<>("A");
+		Node<String> nodeB = new Node<>("B");
+		Node<String> nodeC = new Node<>("C");
+		//node.setValue("A");
+		linkedList.add(nodeA); // é um tipo de variavel
+		linkedList.add(nodeB);
+		linkedList.add(nodeC);
+		
+		Assert.assertTrue(linkedList.getSize() == 3);
+		Assert.assertFalse(linkedList.isEmpty());
+	}
 	
 }
